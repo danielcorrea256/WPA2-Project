f58ad7aa472eeb4976a0911e72f2ba17d94ebda4
Daniel Correa <dancorreaca@unal.edu.co>
2025-02-24 10:38:18 -0500

    proof of concept: krackattack

--- pseudorandom.py
+++ pseudorandom.py
@@ -1,7 +1,7 @@
 import hmac
 import hashlib
 
-def hmac_sha1(key: bytes, message: bytes) -> str:
+def hmac_sha1(key: bytes, message: bytes) -> bytes:
     """
     Returns the HMAC-SHA1 of the given message using the provided key.
     
@@ -9,6 +9,26 @@ def hmac_sha1(key: bytes, message: bytes) -> str:
     :param message: The message to authenticate (as bytes)
     :return:        The HMAC-SHA1 digest (20 bytes)
     """
-    hex_bytes = hmac.new(key, message, hashlib.sha1).digest()
-    hex_str = hex_bytes.hex()
-    return hex_str
+    return hmac.new(key, message, hashlib.sha1).digest()
+
+
+def prf(k : bytes, a : str, b : str, Len : int):
+    """
+    Pseudo-random function (PRF) from the document.
+
+    Args:
+        key (str): The secret key used for HMAC-SHA1.
+        a (str): string input to the PRF.
+        b (str): string input to the PRF.
+        output_length (int): The desired length (in characters) of the final output.
+    Returns:
+        str: The truncated pseudo-random string output of length `output_length`.
+    """
+    R_list = []
+    for i in range((Len + 159) // 160):
+        m = (a + b + str(i)).encode()
+        R_list.append(hmac_sha1(k, m))
+
+    R = b"".join(R_list)
+    bytes_requested = Len // 8
+    return R[:bytes_requested]
--- tests/pseudorandom_test.py
+++ tests/pseudorandom_test.py
@@ -2,10 +2,18 @@ from pseudorandom import hmac_sha1
 
 def test_hmac_sha1():
     """
+    Test Case.
+
+    Verifies the HMAC-SHA1 digest matches the known result from wikipedia:
+        Key = b"key"
+        Data = "The quick brown fox jumps over the lazy dog"
+
+    Expected HMAC-SHA1:
+        de7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9
     """
     key = b"key"
     data = b"The quick brown fox jumps over the lazy dog"
     expected_hex = "de7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9"
     
-    produced_hex = hmac_sha1(key, data)
+    produced_hex = hmac_sha1(key, data).hex()
     assert produced_hex == expected_hex
